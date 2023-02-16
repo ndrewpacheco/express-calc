45574cd65fec85cff537b0257354762549d991e9
Andrew Pacheco <ndrewpacheco@gmail.com>
2023-02-16 01:09:36 -0500

    first ocmmit

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/mean", function (req, res) {
+  const nums = req.query["nums"].split(",");
+  const value = nums.reduce((a, b) => parseInt(a) + parseInt(b)) / nums.length;
+
+  return res.json({
+    response: {
+      operation: "mean",
+      value,
+    },
+  });
+});
+
+app.get("/median", function (req, res) {
+  let nums = req.query["nums"]
+    .split(",")
+    .sort((a, b) => parseInt(a) + parseInt(b));
+  const value = parseInt(nums[Math.round((nums.length - 1) / 2)]);
+
+  return res.json({
+    response: {
+      operation: "median",
+      value,
+    },
+  });
+});
+
+app.listen(3000, function () {
+  console.log("App on port 3000");
+});
