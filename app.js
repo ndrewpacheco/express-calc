d892f54356dd419cbbd328ee86b4140a340ed6cf
Andrew Pacheco <ndrewpacheco@gmail.com>
2023-02-23 01:11:08 -0500

    adding error handling

--- app.js
+++ app.js
@@ -1,20 +1,38 @@
+const CalcError = require("./calcError")
 const express = require("express");
 
 const app = express();
 
+function errorHandler(nums) {
+
+}
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/mean", function (req, res) {
-  const nums = req.query["nums"].split(",");
-  const value = nums.reduce((a, b) => parseInt(a) + parseInt(b)) / nums.length;
+app.get("/mean", function (req, res, next) {
 
-  return res.json({
-    response: {
-      operation: "mean",
-      value,
-    },
-  });
+  try {
+    if (!req.query["nums"]) throw new CalcError('nums are required', 400)
+
+    const nums = req.query["nums"].split(",");
+    for (let i = 0; i < nums.length; i++) {
+      const num = parseInt(nums[i])
+      if (!Number.isInteger(num)) throw new CalcError(`${nums[i]} is not a number`, 404)
+    }
+
+    const value = nums.reduce((a, b) => parseInt(a) + parseInt(b)) / nums.length;
+    return res.json({
+      response: {
+        operation: "mean",
+        value,
+      },
+    });
+
+  } catch (err) {
+    return next(err);
+  }
+
+
 });
 
 app.get("/median", function (req, res) {
---
+++ calcError.js
@@ -0,0 +1,11 @@
+class CalcError extends Error {
+  constructor(message, status) {
+    super();
+    this.message = message;
+    this.status = status;
+    console.error(this.stack);
+  }
+}
+
+
+module.exports = CalcError;
